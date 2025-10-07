b8b1041cdc2aed4b073fb6bf5baf02a75b6510d0
Lihle Magidigidi <lihlethornmagidigidi@gmail.com>
2025-10-07 19:59:48 +0200

    Add Customer model with proper .gitignore

---
+++ CustomerManagementSystem/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerManagementSystem.Models
+{
+    public class Customer
+    {
+        // Model representing a customer to store customer details
+        public int Id { get; set; }
+
+        [Required]  // Name is mandatory for identifying the customer
+        public string Name { get; set; }
+
+        [Required]  // Address is required for records and correspondence
+        public string Address { get; set; }
+
+        public string TelephoneNumber { get; set; }
+
+        public string ContactPersonName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]  // Validate email format for contact person
+        public string ContactPersonEmail { get; set; }
+
+        public string VATNumber { get; set; }
+    }
+}
